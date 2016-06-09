7fb7f4c284776fcbcbc9f69defdd15296424ab58
Sean Harrison <sah@blackearth.us>
2016-06-09 12:29:50 -0500

    NS correction

--- bxml/__init__.py
+++ bxml/__init__.py
@@ -4,7 +4,7 @@ from bl.dict import Dict
 from .xml import XML
 
 NS = Dict(**{
-    "bl": "http:blackearth.us/xml",
+    "bl": "http://blackearth.us/xml",
 })
 
 JARS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'jars')
