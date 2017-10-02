8f6de704e98f123096f7772acfcd94f0c2f896aa
Sean Harrison <sah@blackearth.us>
2017-10-02 13:52:24 -0500

    1.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 config = {
   "name": "bxml",
-  "version": "1.2.0",
+  "version": "1.3.0",
   "description": "XML library",
   "url": "https://github.com/BlackEarth/bxml",
   "author": "Sean Harrison",
