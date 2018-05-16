5f3976992f14500719da5507554fd4d6f6e51a6b
Sean Harrison <sah@bookgenesis.com>
2018-05-16 11:42:56 -0500

    Builder.__call__(tag) will use the builder's element makers with the given namespaced tag

--- bxml/builder.py
+++ bxml/builder.py
@@ -16,9 +16,24 @@ class Builder(Dict):
             nsmap.update(**{k:namespaces[k] for k in namespaces.keys() if namespaces[k] != default})
         else:
             nsmap = namespaces
+        self.__dict__['nsmap'] = nsmap
         for key in namespaces:        # each namespace gets its own method. named for its key
             self[key] = ElementMaker(namespace=namespaces[key], nsmap=nsmap)
         self._ = ElementMaker(namespace=default, nsmap=nsmap)
+
+    def __call__(self, tag, *args, **kwargs):
+        from .xml import XML
+        ns = XML.tag_namespace(tag)
+        keys = list(self.__dict__['nsmap'].keys())
+        values = [self.__dict__['nsmap'][k] for k in keys]
+        if ns is not None and ns in values:
+            key = keys[values.index(ns)]
+            if key is None: key = '_'
+        else:
+            key = '_'
+        name = XML.tag_name(tag)
+        e = self[key](name, *args, **kwargs)
+        return e
 
     @classmethod
     def single(C, namespace=None):
