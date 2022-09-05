ed85a9ff4b042c6ad6554d8e34b09eecf84d5f06
Sean Harrison <sah@kruxia.com>
2022-09-05 10:56:15 -0500

    better default namespaces in bxml.NS - xml, xsi, html

--- bxml/__init__.py
+++ bxml/__init__.py
@@ -2,15 +2,18 @@ from __future__ import print_function
 
 import os
 
-from bl.dict import Dict
-from lxml import etree
+from bl.dict import OrderedDict
 
 from .xml import XML
 
-NS = Dict(
-    **{
-        "bl": "http://blackearth.us/xml",
-    }
+NS = OrderedDict(
+    [
+        # XML
+        ("xml", "http://www.w3.org/XML/1998/namespace"),
+        ("xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+        # XHTML
+        ("html", "http://www.w3.org/1999/xhtml"),
+    ]
 )
 
 PATH = os.path.dirname(os.path.abspath(__file__))
