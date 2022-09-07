72fdddeecc8f185a6ff662281ed1baf47606a1c3
Sean Harrison <sah@kruxia.com>
2022-09-06 20:30:40 -0500

    ElementMaker skips nodes that are None. ("Nodes" rather than "elements" because they can be attribute dicts, strings, or Elements. Very handy!)

--- bxml/element_maker.py
+++ bxml/element_maker.py
@@ -1,26 +1,33 @@
-import lxml.builder
 import types
+
+import lxml.builder
 
 
 class ElementMaker(lxml.builder.ElementMaker):
     """
-    An ElementMaker with the following enhancements:
+    An lxml ElementMaker with enhancements. As with lxml ElementMaker, "nodes" can be
+    strings, dicts, or Elements.
 
-    - `tail` can be defined right in the element definition (saves a lot of trouble).
+    The following enhancements:
+
+    - Nodes can also be lists / tuples / generators, in which case they are unpacked
+      (flattened) into the element. (This is primarily useful for allowing XT
+      transformer methods that take a single element to return a list.)
+    - `tail` keyword can be defined in the element definition (saves a lot of trouble).
     - Attributes must be given as a mapping (dict, etc.) in children (= args), not as
       **kwargs - provide just one right way to add attributes to elements.
-    - [DISABLED] unpack nested lists of children into a single list.
     """
 
-    def __call__(self, tag, *elements, tail=None):
-        def gen_elements(elements):
-            for element in elements:
-                if isinstance(element, (types.GeneratorType, list, tuple)):
-                    for elem in gen_elements(element):
+    def __call__(self, tag, *nodes, tail=None):
+        def gen_nodes(nodes):
+            for node in nodes:
+                if isinstance(node, (types.GeneratorType, list, tuple)):
+                    for elem in gen_nodes(node):
                         yield elem
-                else:
-                    yield element
+                elif node is not None:
+                    yield node
 
-        e = lxml.builder.ElementMaker.__call__(self, tag, *gen_elements(elements))
-        e.tail = tail
-        return e
+        element = lxml.builder.ElementMaker.__call__(self, tag, *gen_nodes(nodes))
+        element.tail = tail
+
+        return element
